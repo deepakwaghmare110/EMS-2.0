6ae0b14eb8fe1863523c1adac0602f4500acd34e
Deepak Waghmare <deepakwaghmare110@gmail.com>
2025-07-28 22:21:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import React, { use } from "react";
+import { useEffect, useState } from "react";
+import Table, { Cell, HeadCell, Row, TBody, THead } from "@atlaskit/table";
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const BASIC_URL = "https://dummyjson.com/users?limit=100";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(BASIC_URL);
+      const data = await res.json();
+      setUsers(data.users);
+      console.log(data);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <Table>
+        <THead>
+          <HeadCell>First Name</HeadCell>
+          <HeadCell>Last Name</HeadCell>
+          <HeadCell>Company</HeadCell>
+          <HeadCell>Blood Group</HeadCell>
+          <HeadCell>Email</HeadCell>
+          <HeadCell>Phone Number</HeadCell>
+        </THead>
+        <TBody>
+          {users.map((user) => (
+            <Row key={user.id}>
+              <Cell>{user.firstName}</Cell>
+              <Cell>{user.lastName}</Cell>
+              <Cell>{user.company.name}</Cell>
+              <Cell>{user.bloodGroup}</Cell>
+              <Cell>{user.email}</Cell>
+              <Cell>{user.phone}</Cell>
+            </Row>
+          ))}
+        </TBody>
+      </Table>
+    </div>
+  );
+}
+
+export default App;
