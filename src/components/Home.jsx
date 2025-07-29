71472a2dd3aeb05d80327123a7fd1b8a93dcc436
Deepak Waghmare <deepakwaghmare110@gmail.com>
2025-07-29 12:40:44 +0530

    commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,26 @@
 import Table, { Cell, HeadCell, Row, TBody, THead } from "@atlaskit/table";
 import Heading from "@atlaskit/heading";
 import Textfield from "@atlaskit/textfield";
+import { useState } from "react";
 
 const Home = ({ users }) => {
+  const [search, setSearch] = useState("");
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
   return (
     <div>
       <div className="heading">
         <Heading size="large">Employee Management System</Heading>
       </div>
       <Textfield
-        className="search-bar"
         type="text"
+        value={search}
+        onChange={handleChange}
         placeholder="Search for employee"
+        className="search-bar"
       />
       <Table>
         <THead>
